24acbab05854d63e7b86b8e55320827e53e77cda
Cameron McHenry <camchenry@users.noreply.github.com>
2021-06-12 17:17:52 -0400

    Switch to netlify

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   appDirectory: "app",
   browserBuildDirectory: "public/build",
-  publicPath: "/_static/build/",
-  serverBuildDirectory: "server/build",
-  devServerPort: 8002
+  publicPath: "/build/",
+  serverBuildDirectory: "build",
+  devServerPort: 8002,
 };
